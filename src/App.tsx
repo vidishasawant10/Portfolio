661b2832852bb1b0fe84011dea3827a06e07b9f4
Vidisha Vijay Sawant <vidishasawantv@gmail.com>
2024-01-31 02:10:40 -0800

    Refactor CSS and remove splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Navbar from './navbar/Navbar';
 import Home from './home/Home';
@@ -8,36 +8,15 @@ import Projects from './projects/Projects';
 import Contact from './contact/Contact';
 
 function App() {
-  const [showSplash, setShowSplash] = useState(true);
-
-  useEffect(() => {
-    // Hide the splash screen after a certain delay
-    const timer = setTimeout(() => {
-      setShowSplash(false);
-    }, 1000); // Change the delay as per your requirement
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="App">
-      {showSplash ? (
-        <div className="SplashScreen">
-          {/* Add your splash screen content here */}
-          <h1>Welcome to My Website!</h1>
-          <p>Loading...</p>
-        </div>
-      ) : (
-        <>
-          <title>Vidisha Portfolio</title>
-          <Navbar />
-          <Home />
-          <About />
-          <Education />
-          <Projects />
-          <Contact />
-        </>
-      )}
+      <title>Vidisha Portfolio</title>
+      <Navbar />
+      <Home />
+      <About />
+      <Education />
+      <Projects />
+      <Contact />
     </div>
   );
 }
