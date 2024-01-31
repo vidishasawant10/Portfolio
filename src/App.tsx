11a4d706769685677501cb56dafd0199cbdbdd90
Vidisha Vijay Sawant <vidishasawantv@gmail.com>
2024-01-31 01:51:38 -0800

    Add smooth scrolling and splash screen to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './navbar/Navbar';
 import Home from './home/Home';
 import About from './about/About';
 import Education from './education/Education';
-
 import Projects from './projects/Projects';
 import Contact from './contact/Contact';
 
-
 function App() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    // Hide the splash screen after a certain delay
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000); // Change the delay as per your requirement
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <title>Vidisha Portfolio</title>
-      <Navbar/>
-      <Home/>
-      <About/>
-      <Education/>
-      <Projects/>
-      <Contact/>
-      
+      {showSplash ? (
+        <div className="SplashScreen">
+          {/* Add your splash screen content here */}
+          <h1>Welcome to My Website!</h1>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <>
+          <title>Vidisha Portfolio</title>
+          <Navbar />
+          <Home />
+          <About />
+          <Education />
+          <Projects />
+          <Contact />
+        </>
+      )}
     </div>
   );
 }
