65b1e0c38dd1018159d54fd1e674f4077ce95fb0
Vidisha Vijay Sawant <vidishasawantv@gmail.com>
2024-02-02 19:22:45 -0800

    Update media queries and text alignment

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -13,18 +13,30 @@ const Navbar: React.FC = () => {
     }
   };
 
+  const closeMenuAndNavigate = (sectionId: string) => {
+    setIsOpen(false);
+    const dropdownMenu = document.querySelector('.dropdown_menu');
+    if (dropdownMenu) {
+      dropdownMenu.classList.remove('open');
+    }
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <>
       <div className="navbar">
         <img src={logo} alt="Logo" className="logoimage" />
         <nav>
           <ul className="links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#education">Education</a></li>
-            <li><a href="#projects">Projects</a></li>
+            <li><a href="#home" onClick={() => closeMenuAndNavigate('home')}>Home</a></li>
+            <li><a href="#about" onClick={() => closeMenuAndNavigate('about')}>About</a></li>
+            <li><a href="#education" onClick={() => closeMenuAndNavigate('education')}>Education</a></li>
+            <li><a href="#projects" onClick={() => closeMenuAndNavigate('projects')}>Projects</a></li>
             <li><a href="https://drive.google.com/file/d/1TaRrZTXcsK6an-29l90MQGoqqbAuPyt3/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#contact" onClick={() => closeMenuAndNavigate('contact')}>Contact</a></li>
           </ul>
         </nav>
         <div className="toggle_btn" onClick={toggleMenu}>
@@ -33,12 +45,12 @@ const Navbar: React.FC = () => {
       </div>
       <div className="dropdown_menu">
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#education">Education</a></li>
-          <li><a href="#projects">Projects</a></li>
+          <li><a href="#home" onClick={() => closeMenuAndNavigate('home')}>Home</a></li>
+          <li><a href="#about" onClick={() => closeMenuAndNavigate('about')}>About</a></li>
+          <li><a href="#education" onClick={() => closeMenuAndNavigate('education')}>Education</a></li>
+          <li><a href="#projects" onClick={() => closeMenuAndNavigate('projects')}>Projects</a></li>
           <li><a href="https://drive.google.com/file/d/1TaRrZTXcsK6an-29l90MQGoqqbAuPyt3/view?usp=sharing" target="_blank" rel="noopener noreferrer">Resume</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="#contact" onClick={() => closeMenuAndNavigate('contact')}>Contact</a></li>
         </ul>
       </div>
     </>
